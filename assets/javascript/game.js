fa40321348f21f5e0e0c25e7d362c2939b87db0d
huntsimon23 <hunt.simon23@gmail.com>
2019-03-18 23:34:34 -0400

    readme pseudocoding, started js

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+// 1. Define Variables (Start Empty): 
+//     - gemValues
+//     - computerValue
+//     - playerScore (Start at 0)
+//     - gemA (is thsi needed?)
+//     - gemB (is thsi needed?)
+//     - gemC (is thsi needed?)
+//     - gemD (is thsi needed?)
+//     - wins (Start at 0)
+//     - losses (Start at 0)
+
+var gemValues = [];
+var computerValue = [];
+var playerScore = 0;
+var wins = 0;
+var losses = 0;
+
+// 2. Define Fuctions 
+//     - startGame 
+//         Assign random computerValue (values btwn 19 and 120)
+//         Display updated computerValue in random-text ID
+//         Empty gemValues array
+//         Generate 4 new Random Numbers and push to gemValues array (values btwn 1 and 12)
+//         Set playerScore = 0
+
+function startGame () {
+    computerValue = (math.random(math.floor) * 1)
+}
