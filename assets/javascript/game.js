649ac74eeb5f82aca737e69220c9877a4f608fb8
huntsimon23 <hunt.simon23@gmail.com>
2019-03-21 18:03:02 -0400

    Js working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,21 +2,17 @@
 //     - gemValues
 //     - computerValue
 //     - playerScore (Start at 0)
-//     - gemA (is thsi needed?)
-//     - gemB (is thsi needed?)
-//     - gemC (is thsi needed?)
-//     - gemD (is thsi needed?)
 //     - wins (Start at 0)
 //     - losses (Start at 0)
-$(); {
-var gemValues = [];
+
+// var gemValues = [];
 var computerValue = [];
 var playerScore = 0;
 var wins = 0;
 var losses = 0;
 
-$("#wins-text").text("wins");
-$("#losses-text").text("losses");
+$(document).ready(function() {
+
 
 // 2. Define Fuctions 
 //     - startGame 
@@ -28,57 +24,74 @@ $("#losses-text").text("losses");
 //         Display playerScore in userscore-text
 
 function startGame() {
-    var min=19; 
-    var max=120;
-    computerValue = math.floor(math.random() * (+max - +min));
-    $("#random-text").text(computerValue);
-    console.log(computerValue);
     playerScore = 0;
-    $(".userscore-text").text(playerScore);
+    $("#userscore-text").text(playerScore);
+    computerValue = [];
+    computerValue = Math.floor(Math.random() * 120) + 19;
+    $("#random-text").text(computerValue);
+    console.log("Computer Value: " + computerValue);
     gemValues = [];
-    while (gemValues.length < 5) {
-        var minG = 1;
-        var maxG = 12;
-        var g = math.floor(math.random() * (+maxG - +minG));
-        gemValues.push[g];
-        console.log(gemValues);
+    while (gemValues.length < 4) {
+        gemValues.push(Math.floor(Math.random() * 12) + 1);
+        console.log("Gem Values: " + gemValues);
     }  
 }
 
 // 3. Call startGame function to assign initial values for first game
 startGame();
 
+function scoreEval() {
+    $("#userscore-text").text(playerScore);
+    if (playerScore === parseInt(computerValue)) {
+        alert("You Win!");
+        wins++;
+        $("#wins-text").text(wins);
+        startGame();
+    } else if (playerScore > parseInt(computerValue)) {
+        alert("You Lose...");
+        losses++;
+        $("#losses-text").text(losses);
+        startGame();
+}}
 // 4. Write JQuery on click functions for each of the 4 gems that add value of Gem(x) to playerScore
 //     CAN I MAKE THIS 1 FUNCTION??? FOR EACH????
-$(".gemA").on("click", function(){
+$("#gemA").on("click", function(){
     playerScore = playerScore + gemValues[0];
-    console.log(playerScore);
+    // $("#userscore-text").text(playerScore);
+    scoreEval();
 })
 
-$(".gemB").on("click", function(){
+$("#gemB").on("click", function(){
     playerScore = playerScore + gemValues[1];
-    console.log(playerScore);
+    // $("#userscore-text").text(playerScore);
+    scoreEval();
 })
 
-$(".gemC").on("click", function(){
+$("#gemC").on("click", function(){
     playerScore = playerScore + gemValues[2];
-    console.log(playerScore);
+    // $("#userscore-text").text(playerScore);
+    scoreEval();
 })
 
-$(".gemA").on("click", function(){
+$("#gemD").on("click", function(){
     playerScore = playerScore + gemValues[3];
-    console.log(playerScore);
+    // $("#userscore-text").text(playerScore);
+    scoreEval();
 })
 // 5. IF/THEN statements to evaluate score
 //         IF playerScore === computerValue, THEN {alert win, update win counter, call startGame function}
 //         IF playerscore > computerValue, THEN {alert lose, update lose counter, call startGame function} 
-if (playerScore === computerValue) {
-    alert("You Win!");
-    wins++;
-    startGame();
+$("#userscore-text").text(playerScore);
+console.log(playerScore);
 
-} else if (playerScore > computerValue) {
-    alert("You Lose...");
-    losses++;
-    startGame();
-}}
+// if (playerScore === parseInt(computerValue)) {
+//     alert("You Win!");
+//     wins++;
+//     startGame();
+// } else if (playerScore > computerValue) {
+//     alert("You Lose...");
+//     losses++;
+//     startGame();
+});
+
+
